235964f9c1c4ede2ac9fd600ef21f176ccef40f9
Guido Zockoll <gzockoll@gmail.com>
2013-03-26 08:02:58 +0100

    requirejs test working

--- src/main/webapp/assets/js/main.js
+++ src/main/webapp/assets/js/main.js
@@ -1,22 +1,18 @@
 requirejs.config({
-	paths: {
-        "bootstrap": "bootstrap",
-        "jquery": "jquery-1.9.1",
-        "highcharts": "highcharts"
-    },
-    shim: {
-        "bootstrap": {
-          deps: ["jquery"],
-          exports: "$.fn.popover"
-        }
-    },
-    enforceDefine: true
+	baseUrl : '../assets/js',
+	urlArgs: "bust=" + (new Date()).getTime(),
+	paths : {
+		'jquery' : 'jquery-1.9.1/jquery.min',
+		'bootstrap' : 'bootstrap/bootstrap.min'
+	},
+	shim : {
+		'bootstrap' : {
+			deps : [ 'jquery' ]
+		}
+	}
 });
 
-require(["jquery","highcharts","helper","charts","init","bootstrap","solarlog_helper","tween-min","steelseries-min"], function($,highcharts,steelseries) {
-    alert("Ready!");
-	//This function is called when scripts/helper/util.js is loaded.
-    //If util.js calls define(), then this function is not fired until
-    //util's dependencies have loaded, and the util argument will hold
-    //the module value for "helper/util".
+requirejs([ 'jquery', 'bootstrap' ], function($, _bootstrap) {
+	alert("Ready!");
+	return {};
 });
